4fb70ebef05a9df7ded16f8039afd40785564c8e
Sahil Pyakurel <sahil.pyakurel@gmail.com>
2023-02-12 20:00:33 -0800

    PythonAnywhere Deploy

--- tightropeAPI/settings.py
+++ tightropeAPI/settings.py
@@ -94,10 +94,17 @@ WSGI_APPLICATION = 'tightropeAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+       'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'sahilp$tightrope',  
+        'USER': 'sahilp',  
+        'PASSWORD': '@Tightrope11',  
+        'HOST': 'sahilp.mysql.pythonanywhere-services.com',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
